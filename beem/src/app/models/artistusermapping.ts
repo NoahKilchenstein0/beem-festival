f6c4caec59f189bce77655d4f0f8bcdca8dc097c
DominikL <dominik.lueckel@t-online.de>
2022-10-21 20:23:30 +0200

    Undo Frontend for recreation with cli
    Co-authored-by: Gilmii <Gilmii@users.noreply.github.com>

--- beem/src/app/models/artistusermapping.ts
+++
@@ -1,8 +0,0 @@
-export class ArtistUserMapping{
-        constructor(init?:Partial<ArtistUserMapping>){
-            Object.assign(this, init);
-        }
-
-    artistId: number = 0;
-    userId: number = 0;
-}
