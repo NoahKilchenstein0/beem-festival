6f30d108af2d2208bf8d7bb0925ff8db63420b31
Noah Kilchenstein <nkilchenstein@gmail.com>
2025-07-12 10:01:46 +0200

    fixed iframe scrolling

--- Beem.Server/ClientApp/src/app/tickets/tickets.component.ts
+++ Beem.Server/ClientApp/src/app/tickets/tickets.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-tickets',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tickets.component.scss']
 })
 export class TicketsComponent implements OnInit {
-
-  constructor() { }
+  iframeHeight = 3000; // Fallback-Wert
 
   ngOnInit(): void {
+    this.adjustIframeHeight();
   }
 
+  @HostListener('window:resize', [])
+  onResize() {
+    this.adjustIframeHeight();
+  }
+
+  adjustIframeHeight(): void {
+    const vh = window.innerHeight;
+
+    // Beispiel: Setze mindestens 2000px, aber nicht weniger als 2x Viewporthöhe
+    this.iframeHeight = Math.max(2000, vh * 2);
+  }
 }
